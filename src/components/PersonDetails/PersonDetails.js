97f8804e9bbc8fb5769ff35f379ef8b8099ffaf4
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-02 23:33:25 +0200

    added basic styles to PersonDetails

--- src/components/PersonDetails/PersonDetails.js
+++ src/components/PersonDetails/PersonDetails.js
@@ -1,9 +1,10 @@
 import React from 'react';
 //import Aux from '../../hoc/Aux';
+import styles from './PersonDetails.module.css';
 
 const personDetails = (props) =>{
     return(
-        <div>
+        <div className = {styles.PersonDetails}>
             <p> First name : {props.name} </p>
             <p> {props.children} </p>
             <input type = "text" onChange = {props.changed}/>
