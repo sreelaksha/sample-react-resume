b46d1fe901e36aaade8ddc2a5a62b94707096be3
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-21 23:39:20 +0200

    removed state from App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, {Component} from 'react';
 import './App.css';
-import PersonDetails from './components/PersonDetails/PersonDetails';
+import PersonalInfo from './components/PersonDetails/PersonalInfo';
 
 class App extends Component {
-    state = {
-            personDetails: [
-                    {firstName: ' '},
-                    {lastName: ' '}
-            ]
-    }
-
-    nameInputHandler = (event) => {
-                this.setState({
-                    personDetails: [
-                                        {firstName: event.target.value},
-                                        {lastName: event.target.value}
-                                ]
-
-                } )
-           }
-
     render(){
         return (
-                <div className="App">
-                    <PersonDetails firstName = {this.state.personDetails[0].firstName}
-                                    lastName = {this.state.personDetails[1].lastName}
-                                    changed = {this.nameInputHandler}/>
+                <div>
+                    <PersonalInfo />
                 </div>
          );
 
