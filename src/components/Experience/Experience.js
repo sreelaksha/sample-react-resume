e8331beecccc553587c2577117c644e29f704108
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-23 23:55:33 +0200

    Added Experience file and its css style

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
+
 import Aux from '../../hoc/Aux';
 import styles from './Experience.module.css';
 import Input from '../Input/Input';
-import { Router, Route, Switch } from "react-router";
+import Button from '../Button/Button';
+
 
 class Experience extends Component {
         state = {
@@ -26,96 +28,19 @@ class Experience extends Component {
         render(){
             return(
                 <Aux>
-                <span className ={styles.PersonalInfoLabel}> PERSONAL INFO </span>
-                <form className ={styles.PersonalInfo}>
+                <span className ={styles.PersonalInfoLabel}> EXPERIENCE </span>
+                <form className ={styles.Experience}>
                       <label className ={styles.Label}>
-                        First name
+                        Employer
                         <input
                           className ={styles.Input}
                           type="text"
-                          name="firstName"
-                          value={this.state.firstName}
+                          name="employer"
+                          value={this.state.employer}
                           onChange={this.handleChange}
                         />
                       </label>
-                      <label className ={styles.Label}>
-                        Last name
-                        <input
-                          className ={styles.Input}
-                          type="text"
-                          name="lastName"
-                          value={this.state.lastName}
-                          onChange={this.handleChange}
-                        />
-                      </label>
-                      <label className ={styles.Label}>
-                          Address
-                          <textarea
-                             className ={styles.Input}
-                            type="text"
-                            name="address"
-                            value={this.state.address}
-                            onChange={this.handleChange}
-                          />
-                        </label>
-                        <label className ={styles.Label}>
-                              City
-                              <input
-                                    className ={styles.Input}
-                                    type="text"
-                                    name="city"
-                                    value={this.state.city}
-                                    onChange={this.handleChange}
-                                  />
-                            </label>
-                            <label className ={styles.Label}>
-                                ZIP Code
-                                <input
-                                  className ={styles.Input}
-                                  type="number"
-                                  name="zipCode"
-                                  value={this.state.zipCode}
-                                  onChange={this.handleChange}
-                                />
-                            </label>
-                            <label className ={styles.Label}>
-                              Country
-                              <select className ={styles.Input} name="country" onChange={this.handleChange} value={this.state.country}>
-                                        <option value="Germany">Germany</option>
-                                        <option value="India">India</option>
-                                        <option value="USA">USA</option>
-                                        <option value="Russia">Russia</option>
-                             </select>
-                            </label>
-                            <label className ={styles.Label}>
-                                Email
-                                <input
-                                  className ={styles.Input}
-                                  type="email"
-                                  name="email"
-                                  value={this.state.email}
-                                  onChange={this.handleChange}
-                                />
-                            </label>
-                            <label className ={styles.Label}>
-                                Phone
-                                <input
-                                  className ={styles.Input}
-                                  type="number"
-                                  name="phone"
-                                  value={this.state.phone}
-                                  onChange={this.handleChange}
-                                />
-                            </label>
-                      <p> Your firstName is: {this.state.firstName}</p>
-                      <p> Your lastName is: {this.state.lastName}</p>
-                      <p> Your address is: {this.state.address}</p>
-                      <p> Your city is: {this.state.city}</p>
-                      <p> Your ZipCode is: {this.state.zipCode}</p>
-                      <p> Your country is: {this.state.country}</p>
-                      <p> Your email is: {this.state.email}</p>
-                      <p> Your phone is: {this.state.phone}</p>
-                      <button className ={styles.Button}> NEXT </button>
+                      <Button> NEXT </Button>
                     </form>
                  </Aux>
             );
