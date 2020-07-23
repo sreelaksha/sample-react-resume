e631c44125868a5ddcef1456522715bde3812d52
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-23 23:00:38 +0200

    imported BrowserRouter, Switch from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {Component} from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import PersonalInfo from './components/PersonDetails/PersonalInfo';
+
 
 class App extends Component {
     render(){
         return (
-                <div>
-                    <PersonalInfo />
-                </div>
+                <BrowserRouter>
+                    <Switch>
+                           <Route path = "/" exact component = {PersonalInfo} />
+                    </Switch>
+                </BrowserRouter>
          );
 
     }
