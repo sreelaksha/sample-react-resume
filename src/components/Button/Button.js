df0888f23f4d62c6497f4e59c1bc1f185e64adaf
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-29 17:09:26 +0200

    fixed the Form not connected & history.goBack() error by giving button type = "button"

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,13 +2,12 @@ import React from 'react';
 import styles from './Button.module.css';
 
     const button = (props) => (
-        <div className = {styles.ButtonDiv}>
-            <button
+        <button
+                type="button"
                 className = {styles.Button}
                 onClick = {props.clicked} >
                 {props.children}
             </button>
-        </div>
-    );
+     );
 
 export default button;
