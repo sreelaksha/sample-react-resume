73919aa3b09a233e6898acb5ec7f0629beac2aa0
Sreelaksha <sreelaksha.s@gmail.com>
2020-09-10 16:09:55 +0200

    changed the href link for frontPage

--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -5,7 +5,7 @@ const frontPage = (props) => (
     <div className = {styles.FrontPage}>
     <nav>
                <p>Create your professional resume in just few minutes
-                    <a href="/">Build My Resume</a>
+                    <a href="/personalInfo">Build My Resume</a>
                 </p>
             </nav>
 
