483002a99810c854f605dc8f745ff54c728cb3f9
Sreelaksha <sreelaksha.s@gmail.com>
2020-09-21 20:58:59 +0200

    using useEffect in Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,9 @@ import React from 'react';
 import styles from './Button.module.css';
 
     const button = (props) => (
+    useEffect(( =>{
+            console.log('Button - useEffect');
+    }))
         <button
                 type="button"
                 className = {styles.Button}
