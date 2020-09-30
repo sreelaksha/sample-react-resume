59ef8799c2361ce4e47ef394658709cbda40fc53
Sreelaksha <sreelaksha.s@gmail.com>
2020-09-30 12:25:47 +0200

    trying to check if Object keys work

--- src/components/PersonalInfoSummary/PersonalInfoSummary.js
+++ src/components/PersonalInfoSummary/PersonalInfoSummary.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Aux from '../hoc/Aux';
 
 const personalInfoSummary = (props) => {
+    const personalInfoFormSummary = Object.keys(props.personalInfoForm)
+    console.log(props.personalInfoForm);
     return (
         <Aux>
             <p> Your Personal Info Summary </p>
