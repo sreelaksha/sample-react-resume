b342c50ee810f30a59fb9e9c3bc7dd743da2a9d1
Sreelaksha <sreelaksha.s@gmail.com>
2020-09-30 12:01:43 +0200

    added new file to show summary of PersonalInfo details

---
+++ src/components/PersonalInfoSummary/PersonalInfoSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Aux from '../hoc/Aux';
+
+const personalInfoSummary = (props) => {
+    return (
+        <Aux>
+            <p> Your Personal Info Summary </p>
+        </Aux>
+
+    );
+}
+
+export default personalInfoSummary;
