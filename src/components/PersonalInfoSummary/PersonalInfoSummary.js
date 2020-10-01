bf7a064600069940309ab2603bff65bc62f4712f
Sreelaksha <sreelaksha.s@gmail.com>
2020-10-01 12:23:23 +0200

    added props for PersonalInfoSummary

--- src/components/PersonalInfoSummary/PersonalInfoSummary.js
+++ src/components/PersonalInfoSummary/PersonalInfoSummary.js
@@ -8,6 +8,13 @@ const personalInfoSummary = (props) => {
     return (
         <Aux>
             <p> Your Personal Info Summary </p>
+              <p> Your First name is: {props.firstName}</p>
+                <p> Your Last name is: {props.lastName}</p>
+                <p> Your Email is: {props.email}</p>
+                <p> Your Address is: {props.address}</p>
+                <p> Your City is: {props.city}</p>
+                <p> Your Country is: {props.country}</p>
+                <p> Your Phone is: {props.phone}</p>
         </Aux>
 
     );
