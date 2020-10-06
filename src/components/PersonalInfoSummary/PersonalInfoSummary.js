fadef0a9f7839205e04791e91f1b3a904b5d2ac1
Sreelaksha <sreelaksha.s@gmail.com>
2020-10-06 16:04:40 +0200

    adding the Personal info summary modal

--- src/components/PersonalInfoSummary/PersonalInfoSummary.js
+++ src/components/PersonalInfoSummary/PersonalInfoSummary.js
@@ -4,17 +4,15 @@ import Aux from '../../hoc/Aux';
 
 const personalInfoSummary = (props) => {
     const personalInfoFormSummary = Object.keys(props.personalInfoForm)
-    console.log('Hi personalInfoSummary');
+        .map(infoKey => {
+                return <p> {props.personalInfoForm[infoKey]} </p>
+        });
+    //console.log(props.personalInfoForm);
     return (
         <Aux>
             <p> Your Personal Info Summary </p>
-              <p> Your First name is: {props.firstName}</p>
-                <p> Your Last name is: {props.lastName}</p>
-                <p> Your Email is: {props.email}</p>
-                <p> Your Address is: {props.address}</p>
-                <p> Your City is: {props.city}</p>
-                <p> Your Country is: {props.country}</p>
-                <p> Your Phone is: {props.phone}</p>
+           <div> {personalInfoFormSummary} </div>
+
         </Aux>
 
     );
