408e0709a3e0b39aa42fa38c5d7065c423dd72f1
Sreelaksha <sreelaksha.s@gmail.com>
2020-10-08 16:08:04 +0200

    added some placeholders in Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
         <header className = {styles.Toolbar}>
-            <DrawerToggle clicked= {props.drawerToggleClicked} />
-            <div className = {styles.Logo}>
-                <Logo />
-            </div>
-            <nav className ={styles.DesktopOnly}>
-                <NavigationItems />
+            <div>MENU</div>
+            <div>LOGO</div>
+            <nav>
+                ...
             </nav>
         </header>
 );
