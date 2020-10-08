b4608f0cad60c9bee1ebc675c568e1ed5ac61c7a
Sreelaksha <sreelaksha.s@gmail.com>
2020-10-08 19:38:43 +0200

    imported toolbar in FrontPage

--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './FrontPage.module.css';
+import Toolbar from '../Toolbar/Toolbar';
 
 const frontPage = (props) => (
     <div className = {styles.FrontPage}>
-    <nav>
+    <Toolbar/><nav>
                <p>Create your professional resume in just few minutes
                     <a href="/personalInfo">Build My Resume</a>
                 </p>
