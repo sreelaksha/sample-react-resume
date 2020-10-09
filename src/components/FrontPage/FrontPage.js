b6daa6842dc7ab7ad908cfe50366ba7ebc95e72e
Sreelaksha <sreelaksha.s@gmail.com>
2020-10-09 20:55:34 +0200

    imported Button component in FrontPage

--- src/components/FrontPage/FrontPage.js
+++ src/components/FrontPage/FrontPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './FrontPage.module.css';
 import Toolbar from '../Toolbar/Toolbar';
+import Button from '../Button/Button';
 
 const frontPage = (props) => (
     <div className = {styles.FrontPage}>
     <Toolbar/><nav>
                <p>Create your professional resume in just few minutes</p>
-                    <a href="/personalInfo">Build My Resume</a>
+                    <Button ><a className= {styles.button} href="/personalInfo">Build My Resume</a></Button>
 
             </nav>
 
