4c22e3dcb7a5e7c23d3248785d0121c025ffd3e1
Sreelaksha <sreelaksha.s@gmail.com>
2020-10-09 16:20:33 +0200

    imported sign in image in Toolbar

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../Logo/Logo';
+import signInLogo from '../../assets/signin.png'
 
 const toolbar = (props) => (
         <header className = {styles.Toolbar}>
             <Logo/>
             <div>MENU</div>
             <nav>
-                ...
+                <img src = {signInLogo} alt = "Sign-In"/>
             </nav>
         </header>
 );
